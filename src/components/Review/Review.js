1eddcd8cdca08a36e2598bf78759f05d92ee875b
mabrur007 <mirmd.mabrur@gmail.com>
2022-04-05 18:12:58 +0600

    style added to review page

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Review.css';
 
 const Review = ({ review }) => {
-    const { name, id } = review;
+    const { img, name, says, ratings } = review;
+    
     return (
-        <div>
+        <div className='review'>
+            <img src={img} alt="reviewer" />
             <h4>Name:{name}</h4>
-            <p>id: { id}</p>
+            <p> {says}</p>
+            <p>Ratings: <b>{ ratings}</b></p>
         </div>
     );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import useReviews from '../../hooks/useReviews';
 import Review from '../Review/Review';
+import './Reviews.css';
 
 const Reviews = () => {
     const [reviews] = useReviews();
     return (
         <div>
-            <h2>This is Review page.</h2>
-            <div>
+            <h2 className='review-title'>Customer <span>Reviews</span></h2>
+            <div className='review-section'>
                 {
                     reviews.map(review => <Review key={review.id} review={review}></Review>)
                 }
