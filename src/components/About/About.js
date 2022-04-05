5fd4f7aeb1ea13ec7870084cf9fddadbc443382f
mabrur007 <mirmd.mabrur@gmail.com>
2022-04-05 20:40:44 +0600

    home page review added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,6 +5,9 @@ const About = () => {
     return (
         <div>
             <h2 className='about-title'>About <span>Us</span></h2>
+            <div className='about-content'>
+                <p>We are the best drone service provider in Bangladeshi market. You can keep your faith on us. What ever the service is- <span>buy, sell, exchange, repair</span> or anything regarding drone, we are the one stop solution.</p>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import useReviews from "../../hooks/useReviews";
+import Review from "../Review/Review";
+import Reviews from "../Reviews/Reviews";
 import './Home.css';
 
 const Home = () => {
+  const [reviews] = useReviews();
   return (
     <div>
       <div className="banner-area">
@@ -14,12 +19,36 @@ const Home = () => {
             in Bangladesh. We have been in this field for last 10 years. Through
             out the time span we have successfully satisfied thousands of
             customers with our services. So what are you waiting for? Dive in to
-            become a <span><b>Drone Buddy</b></span>!
+            become a{" "}
+            <span>
+              <b>Drone Buddy</b>
+            </span>
+            !
           </p>
-          <button className="content-btn">Learn More</button>
+          <Link to='/about'>
+            <button className="content-btn">Learn More</button>
+          </Link>
         </div>
         <div className="img-area">
-            <img src="drone.jpg" alt="drone-img" />
+          <img src="drone.jpg" alt="drone-img" />
+        </div>
+      </div>
+
+      {/* Customer Review area */}
+      <div className="home-review-section">
+        <h2 className="review-title">
+          Customer <span>Reviews</span>
+        </h2>
+
+        <div className="review-section">
+          {reviews.slice(0, 3).map((review) => (
+            <Review key={review.id} review={review}></Review>
+          ))}
+        </div>
+        <div>
+          <Link to="/reviews">
+            <button className="review-btn">See All Reviews</button>
+          </Link>
         </div>
       </div>
     </div>
